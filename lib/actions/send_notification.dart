ab067991590f02d45c94ffeb62f24cbaefce65b8
Jawwaad <jawwaad.sabree01@gmail.com>
2023-06-13 19:52:55 -0400

    changed host to deployed service

--- lib/actions/send_notification.dart
+++ lib/actions/send_notification.dart
@@ -1,9 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+// const host = 'http://localhost:3000';
+const host = 'https://push-notification-service.onrender.com';
+
 Future<void> sendNotification(String message, List<String> devices) async {
   try {
-    final url = Uri.parse('http://localhost:3000/new-comment');
+    final url = Uri.parse('$host/new-comment');
     await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
